ac4bebedaff90ce7ee1551cd3dc08ed54ab83d07
Roshan Shah <roshanshah639@gmail.com>
2025-03-07 07:44:47 +0545

    intall react.js and tailwindcss & create the first template

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-2xl font-semibold text-red-500">Home Page</h1>
+    </div>
+  );
+};
+
+export default App;
